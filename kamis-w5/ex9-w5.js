96e65f07369d2fab9e525b18ce67eb74249feebc
damaradani <danny12march@gmail.com>
2018-02-20 21:50:00 +0700

    Initiate folder kamis-w5 untuk file exercise 8 & 9 week 5

---
+++ kamis-w5/ex9-w5.js
@@ -0,0 +1,24 @@
+//Exercise 9 week 5 Logic Challenge - Cari Pelaku (Regex)
+
+/*Problem
+Diberikan sebuah function cariPelaku(kalimat) yang menerima satu parameter
+berupa string. Function akan me-return jumlah berapa kali ditemukan
+kata "abc" secara berturut-turut di dalam kalimat tersebut.
+Gunakan regex untuk melatih kemampuan regex.
+*/
+
+function cariPelaku(str) {
+  var pelaku = str.match(/abc/g);
+  return pelaku.length;
+}
+
+/*function cariPelaku(str) {
+  // you can only write your code here!
+}*/
+
+// TEST CASES
+console.log(cariPelaku('mabcvabc')); // 2
+console.log(cariPelaku('abcdabdc')); // 1
+console.log(cariPelaku('bcabcac')); // 1
+console.log(cariPelaku('abcabcabc')); // 3
+console.log(cariPelaku('babcbacabc')); // 2
